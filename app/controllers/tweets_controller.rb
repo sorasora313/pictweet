f5272b9d5b70237b401e8663c65fb0ce5a5d084a
sorasora313 <sky.313.sora@gmail.com>
2022-09-16 21:21:45 +0900

    ユーザ関係機能

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,8 +1,9 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: [:edit, :show]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.includes(:user).order("created_at DESC")
   end
 
   def new
@@ -30,12 +31,17 @@ class TweetsController < ApplicationController
   end
 
   private
-
   def tweet_params
-    params.require(:tweet).permit(:name, :image, :text)
+    params.require(:tweet).permit(:image, :text).merge(user_id: current_user.id)
   end
 
   def set_tweet
     @tweet = Tweet.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
